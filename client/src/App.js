671cdfa95238c0ca7792d3331956c158e4269a9e
wip-abramson <wip.abramson@gmail.com>
2021-07-16 18:23:51 +0100

    Add report documents

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ const EXPRESS_HOST = process.env.REACT_APP_EXPRESS_HOST || "localhost"
 function App() {
 
     let [dbConnection, setDbConnection] = React.useState(null)
-    let [counterDocument, setCounterDocument] = React.useState(null)
+    let [reportDocument, setReportDocument] = React.useState(null)
     let [count, setCount] = React.useState(null)
 
 
@@ -28,113 +28,75 @@ function App() {
         let connection = new ShareDb.Connection(socket);
         setDbConnection(connection)
         // Create local Doc instance mapped to 'examples' collection document with id 'counter'
-        let counterDoc = connection.get('examples', 'counting');
+        let reportDoc = connection.get('report', 'https://w.rubric.cc/reports/1');
 
-        console.log(counterDoc)
-        setCounterDocument(counterDoc)
-        console.log("LATEST VERSION ", counterDoc.version)
+        console.log(reportDoc)
+        setReportDocument(reportDoc)
+        console.log("LATEST VERSION ", reportDoc.version)
 
-        setLatestVersion(counterDoc.version)
+        setLatestVersion(reportDoc.version)
 
 
     }, [])
 
     React.useEffect(() => {
-        console.log("ATTEMPTING TO SET COUNTER DOC", counterDocument)
-        if (counterDocument) {
+        console.log("ATTEMPTING TO SET COUNTER DOC", reportDocument)
+        if (reportDocument) {
             console.log("Document set")
             // Get initial value of document and subscribe to changes
-            counterDocument.subscribe(updateNumClicks);
-            // When document changes (by this client or any other, or the server),
-            // update the number on the page
-            counterDocument.on('op', updateNumClicks);
+            reportDocument.subscribe(updateNumClicks);
+            // // When document changes (by this client or any other, or the server),
+            // // update the number on the page
+            // counterDocument.on('op', updateNumClicks);
+          console.log("subscrived", reportDocument)
 
 
         }
 
-    }, [counterDocument])
+    }, [reportDocument])
 
     function updateNumClicks() {
 
-        setCount(counterDocument.data.count)
-        setLatestVersion(counterDocument.version)
+        // setCount(reportDocument.data.count)
+        // setLatestVersion(reportDocument.version)
     }
 
     // When clicking on the '+1' button, change the number in the local
     // document and sync the change to the server and other connected
     // clients
-    function increment() {
-        // https://github.com/ottypes/json0 for list of valid operations.
-        counterDocument.submitOp([{p: ['count'], na: 1}], (error) => {
-            console.log("Version Updated:", counterDocument.version)
+    // function increment() {
+    //     // https://github.com/ottypes/json0 for list of valid operations.
+    //     reportDocument.submitOp([{p: ['count'], na: 1}], (error) => {
+    //         console.log("Version Updated:", reportDocument.version)
+    //
+    //         setLatestVersion(reportDocument.version)
+    //     });
+    // }
+    //
+    // function decrement() {
+    //     // https://github.com/ottypes/json0 for list of valid operations.
+    //     reportDocument.submitOp([{p: ['count'], na: -1}], (error) => {
+    //         console.log("Version Updated:", reportDocument.version)
+    //
+    //         setLatestVersion(reportDocument.version)
+    //     });
+    // }
 
-            setLatestVersion(counterDocument.version)
-        });
-    }
-
-    function decrement() {
-        // https://github.com/ottypes/json0 for list of valid operations.
-        counterDocument.submitOp([{p: ['count'], na: -1}], (error) => {
-            console.log("Version Updated:", counterDocument.version)
-
-            setLatestVersion(counterDocument.version)
-        });
-    }
-
-    function fetchPreviousVersion(e) {
-        e.preventDefault()
-
-        if (!isNaN(previousVersion)) {
-            console.log(previousVersion)
-            let versionToFetch = parseInt(previousVersion)
-
-            if (versionToFetch < latestVersion) {
-                dbConnection.fetchSnapshot("examples", "counting", versionToFetch, (error, snapshot) =>  {
-                    console.log(snapshot)
-                    console.log("Version", snapshot.v)
-
-                    setPreviousSnapshot(snapshot)
-                })
-            }
-            else {
-                alert("You must only search earlier versions")
-            }
-
-
-        }
-        else {
-            alert("Version Must Be a Number")
-        }
-    }
 
   return (
     <div className="App">
       <header className="App-header">
-          {counterDocument ?
+          {reportDocument ?
+
               <div style={{"font-family": "Helvetica Neue, Helvetica, Arial, sans-serif", "font-size": "36px"}}>
                   <h2>Current Count</h2>
                   <div>Version {latestVersion}</div>
               Count {count ? <span id="num-clicks">{count}</span> : "loading"} times.
 
-                  <button style={{"font-size": "36px"}} onClick={decrement}>-1</button>
-                  <button style={{"font-size": "36px"}} onClick={increment}>+1</button>
+                  {/*<button style={{"font-size": "36px"}} onClick={decrement}>-1</button>*/}
+                  {/*<button style={{"font-size": "36px"}} onClick={increment}>+1</button>*/}
 
-                  <div>
-                      <h3>Fetch Previous Count</h3>
-                      <form onSubmit={fetchPreviousVersion}>
-                          <div><span>Version:</span><input value={previousVersion} onChange={(e) => setPreviousVersion(e.target.value)}/></div>
-                          <button type="submit">Fetch</button>
-                      </form>
 
-                  </div>
-
-                  {previousSnapshot &&
-                    <div>
-                        <h3>Previous Version</h3>
-                        <div>Version: {previousSnapshot.v}</div>
-                        <div>Count: {previousSnapshot.data.count}</div>
-                    </div>
-                  }
 
           </div> :
               <div>...Loading</div>
--- server/index.js
+++ server/index.js
@@ -5,45 +5,60 @@ var ShareDB = require('sharedb')
 var path = require("path")
 var WebSocketJSONStream = require('@teamwork/websocket-json-stream')
 require('dotenv').config();
+const fs = require('fs');
+
+
+let criteria = JSON.parse(fs.readFileSync('./data/criteria.json'));
+let evaluations = JSON.parse(fs.readFileSync('./data/evaluations.json'));
+let evaluators = JSON.parse(fs.readFileSync('./data/evaluators.json'));
+let methods = JSON.parse(fs.readFileSync('./data/methods.json'));
+let reports = JSON.parse(fs.readFileSync('./data/reports.json'));
+let rubrics = JSON.parse(fs.readFileSync('./data/rubrics.json'));
+let sources = JSON.parse(fs.readFileSync('./data/sources.json'));
+
 
 const PORT = process.env.EXPRESS_PORT || 8000;
 const DB_PORT = process.env.DB_PORT || 27017
 
-const db = require('sharedb-mongo')(`mongodb://localhost:${DB_PORT}`, {mongoOptions: { useUnifiedTopology: true}});
+const db = require('sharedb-mongo')(`mongodb://localhost:${DB_PORT}`, {mongoOptions: {useUnifiedTopology: true}});
 const backend = new ShareDB({db});
-createDoc(startServer);
+initialiseDb(startServer);
 
 // Create initial document then fire callback
-function createDoc(callback) {
-    var connection = backend.connect();
-    var doc = connection.get('examples', 'counting');
-    doc.fetch(function(err) {
-        if (err) throw err;
-        if (doc.type === null) {
-            console.log("creating document")
-            doc.create({count: 0}, callback);
-            return;
-        }
-        callback();
+function initialiseDb(callback) {
+  var connection = backend.connect();
+
+  for (report in reports) {
+    var reportDoc = connection.get('report', report.id)
+    reportDoc.fetch(function (err) {
+      if (err) throw err;
+      if (reportDoc.type === null) {
+        console.log("creating document")
+        reportDoc.create(report, () => {
+        });
+        return;
+      }
     });
+  }
+
+  callback();
 }
 
 function startServer() {
-    // Create a web server to serve files and listen to WebSocket connections
-    var app = express();
-    app.use(express.static(path.join(__dirname, "..", "client", "build")));
-    app.use(express.static('static'));
-    var server = http.createServer(app);
+  // Create a web server to serve files and listen to WebSocket connections
+  var app = express();
+  app.use(express.static(path.join(__dirname, "..", "client", "build")));
+  app.use(express.static('static'));
+  var server = http.createServer(app);
 
 
+  // Connect any incoming WebSocket connection to ShareDB
+  var wss = new WebSocket.Server({server: server});
+  wss.on('connection', function (ws) {
+    var stream = new WebSocketJSONStream(ws);
+    backend.listen(stream);
+  });
 
-    // Connect any incoming WebSocket connection to ShareDB
-    var wss = new WebSocket.Server({server: server});
-    wss.on('connection', function(ws) {
-        var stream = new WebSocketJSONStream(ws);
-        backend.listen(stream);
-    });
-
-    server.listen(PORT);
-    console.log('Listening on http://localhost:' + PORT);
+  server.listen(PORT);
+  console.log('Listening on http://localhost:' + PORT);
 }
