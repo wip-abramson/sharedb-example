15678dd082fcf56c7d7ea13ae750aea903ec73b9
Joe Andrieu <joe@legreq.com>
2021-07-19 10:28:58 +0000

    added Debug for index.js, error handling for db load, db now loading

--- server/index.js
+++ server/index.js
@@ -7,30 +7,43 @@ var WebSocketJSONStream = require('@teamwork/websocket-json-stream')
 require('dotenv').config();
 const fs = require('fs');
 
+const Debug = require('debug');
+const debug = Debug('index.js');
+debug('PWD', process.env.PWD);
+var criteria, evaluations, evaluators, methods, reports, rubrics, sources;
 
-let criteria = JSON.parse(fs.readFileSync('./data/criteria.json'));
-let evaluations = JSON.parse(fs.readFileSync('./data/evaluations.json'));
-let evaluators = JSON.parse(fs.readFileSync('./data/evaluators.json'));
-let methods = JSON.parse(fs.readFileSync('./data/methods.json'));
-let reports = JSON.parse(fs.readFileSync('./data/reports.json'));
-let rubrics = JSON.parse(fs.readFileSync('./data/rubrics.json'));
-let sources = JSON.parse(fs.readFileSync('./data/sources.json'));
-
+try {
+  criteria = JSON.parse(fs.readFileSync('./data/criteria.json'));
+  evaluations = JSON.parse(fs.readFileSync('./data/evaluations.json'));
+  evaluators = JSON.parse(fs.readFileSync('./data/evaluators.json'));
+  methods = JSON.parse(fs.readFileSync('./data/methods.json'));
+  reports = JSON.parse(fs.readFileSync('./data/reports.json'));
+  rubrics = JSON.parse(fs.readFileSync('./data/rubrics.json'));
+  sources = JSON.parse(fs.readFileSync('./data/sources.json'));
+} catch (e) {
+  console.log('Failed to load data from data dir', e);
+}
 
 const PORT = process.env.EXPRESS_PORT || 8000;
 const DB_PORT = process.env.DB_PORT || 27017
 
+debug('PORT:    ', PORT);
+debug('DB_PORT: ', DB_PORT);
+
+debug('report[0]', reports[0]);
 const db = require('sharedb-mongo')(`mongodb://localhost:${DB_PORT}`, {mongoOptions: {useUnifiedTopology: true}});
-const backend = new ShareDB({db});
+const backend = new ShareDB({db}); 
 initialiseDb(startServer);
 
 // Create initial document then fire callback
 function initialiseDb(callback) {
   var connection = backend.connect();
 
-  for (report in reports) {
+  reports.forEach(function(report){
+    debug('fetching report', report);
     var reportDoc = connection.get('report', report.id)
     reportDoc.fetch(function (err) {
+    
       if (err) throw err;
       if (reportDoc.type === null) {
         console.log("creating document")
@@ -38,8 +51,8 @@ function initialiseDb(callback) {
         });
         return;
       }
-    });
-  }
+    }) 
+  });
 
   callback();
 }
